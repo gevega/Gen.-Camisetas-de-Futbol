90262546cd422fac10b25a3ef98d7a8fe4935404
Gonzalo Vega <gevegaecheverria@gmail.com>
2022-10-07 16:50:42 -0300

    Carrito de compras y arreglos de menu

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,13 @@
 let produ = document.querySelector('body')
 let productos = []
+let container = document.querySelector('.cart-items')
+let total = document.querySelector('.total')
+let totalcart = 0
 
 loadEventList()
 function loadEventList() {
     produ.addEventListener('click', addPro)
+    container.addEventListener('click', remove)
 }
 
 function addPro(e) {
@@ -11,34 +15,107 @@ function addPro(e) {
         const selectPro = e.target.parentElement
         readContent(selectPro)
     }
+    cart_logo(productos)
+}
+
+function remove(e) {
+    if (e.target.classList.contains('remove')) {
+        const idProdu = e.target.getAttribute('data-id')
+
+        productos.forEach(value => {
+            if (value.id == idProdu) {
+                let removePre = parseFloat(value.precio) * parseFloat(value.cantidad)
+                totalcart = totalcart - removePre
+                totalcart = totalcart.toFixed(0)
+            }
+        })
+        productos = productos.filter(productos => productos.id != idProdu)
+    }
+
+    if (productos.length == 0) {
+        total.innerHTML = 'Total: $0'
+        totalcart=0 
+    } 
+    loadHTML()
 }
 
 function readContent(product) {
     const infoProduct = {
         id: product.querySelector('.buy').getAttribute('data-id'),
-        imagen: product.querySelector('div img').src,
+        imagen: product.querySelector('.produ').src,
         titulo: product.querySelector('h2').textContent,
-        precio: product.querySelector('.pre').textContent,
+        precio: product.querySelector('.precio').textContent,
         cantidad: 1
     }
-    productos = [...productos, infoProduct]
-    loadHTML();
-    console.log(infoProduct)
 
+    totalcart = parseFloat(totalcart) + parseFloat(infoProduct.precio)
+
+    const repeat = productos.some(product => product.id == infoProduct.id)
+    if (repeat) {
+        const list = productos.map(product => {
+            if (product.id == infoProduct.id) {
+                product.cantidad++
+                return product
+            } else {
+                return product
+            }
+        })
+        productos = [...list]
+    } else {
+        productos = [...productos, infoProduct]
+    }
+    loadHTML()
+    //save_localstorage(infoProduct)
 }
 
 function loadHTML() {
+    clearhtml()
     productos.forEach(product => {
         const { id, imagen, titulo, precio, cantidad } = product;
         const fila = document.createElement('div')
-        fila.classList.add('grid-item')
+        fila.classList.add('item')
         fila.innerHTML = `
-                <div class="grid-img">
-                    <img src="${imagen}" alt="ALEMANIA 2022" class="produ">
-                    <div class="pre">${precio}</div>
-                </div>
-                <h2 id="tittle-pro">${titulo}</h2>
-                <h2 id="tittle-pro">Cantidad: ${cantidad}</h2>
-            `
+            <div class="logo-item">
+                <img src="${imagen}" alt="">
+            </div>
+            <div class="info-item">
+                <h4>${titulo}</h4>
+                <h5>Precio: ${precio}</h3>
+                <h5>Cantidad: ${cantidad}</h3>
+            </div>
+            <div class="remove">
+                <img src="img/svg/remove.svg" class="remove" data-id="${id}" alt="">
+            </div>
+            `;
+        container.appendChild(fila)
+        total.innerHTML = 'Total: $' + totalcart
     })
 }
+
+function clearhtml() {
+    container.innerHTML = ''
+}
+
+function cart_logo(productos){
+    let cart = document.querySelector(".cart").parentElement.parentElement
+    if(productos.length != 0){
+        cart.innerHTML= `
+        <a>
+            <img src="img/svg/cart-add.svg" alt="Logo" class="cart">
+        </a>
+        `
+    }else{
+        cart.innerHTML= `
+        <a>
+            <img src="img/svg/cart.svg" alt="Logo" class="cart">
+        </a>
+        `
+    }
+}
+
+
+
+
+/*function save_localstorage(infoProduct){ 
+    localStorage.setItem('Producto '+infoProduct.id, JSON.stringify(infoProduct))
+}*/
--- js/colapse-cart.js
+++ js/colapse-cart.js
@@ -1,14 +1,46 @@
-
 let cart = document.querySelector('nav')
+let colapse = document.getElementById('colapse-cart')
+let menu = document.getElementById('items')
+let vw = document.documentElement.clientWidth
+let vh = document.documentElement.clientHeight
 
 loadEventLis()
 function loadEventLis() {
-    cart.addEventListener('click', text)
+    cart.addEventListener('click', show_cart)
 }
 
-function text(e) {
+function show_cart(e) {
     if (e.target.classList.contains('cart')) {
-        const selectPro = e.target.parentElement
-        console.log('Hola paps')
+        if (colapse.style.display == "flex") {
+            colapse.style.display = 'none'
+            console.log('Ocultar')
+        } else {
+            colapse.style.display = 'flex'
+            if (vw <= 640) {
+                menu.style.display = 'none'
+            } else { menu.style.display = 'flex' }
+            console.log('Mostrar')
+        }
+    }
+
+    if (e.target.classList.contains('svg-icon')) {
+        if (menu.style.display == "flex") {
+            menu.style.display = 'none'
+            console.log('Ocultar')
+        } else {
+            menu.style.display = 'flex'
+            colapse.style.display = 'none'
+            console.log('Mostrar')
+        }
     }
 }
+
+/*function normalizeHTML() {
+    vw = document.documentElement.clientWidth
+    vh = document.documentElement.clientHeight
+    if (vw >= 640 || vh >= 1140) {
+        menu.style.display = 'flex'
+    } else {
+        menu.style.display = 'none'
+    }
+}*/
